8f5ddb396523241d68fa28f3537a754a0ef64b62
Andras Mihaly <andrasm96@gmail.com>
2019-11-18 17:53:31 -0800

    handlers.go: Add file to directory

---
+++ cmd/tradewars/handlers.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+    "fmt"
+    "html/template" 
+    "log"           
+    "net/http"
+    "strconv"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path != "/" {
+        http.NotFound(w, r)
+        return
+    }
+
+    ts, err := template.ParseFiles("./ui/html/home.page.tmpl")
+    if err != nil {
+        log.Println(err.Error())
+        http.Error(w, "Internal Server Error", 500)
+        return
+    }
+
+    err = ts.Execute(w, nil)
+    if err != nil {
+        log.Println(err.Error())
+        http.Error(w, "Internal Server Error", 500)
+    }
+}
