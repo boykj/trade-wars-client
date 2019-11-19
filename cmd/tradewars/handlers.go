1fae2b0ed1c9eddf0200d7e77ecfcdc2fef3294f
boykj <jboyk918@gmail.com>
2019-11-19 12:31:57 -0800

    main.go: || handlers.go: Refactor handlers into handlers.go file

--- cmd/tradewars/handlers.go
+++ cmd/tradewars/handlers.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-    "fmt"
-    "html/template" 
-    "log"           
+    "fmt"        
     "net/http"
     "strconv"
 )
@@ -26,4 +24,24 @@ func home(w http.ResponseWriter, r *http.Request) {
         log.Println(err.Error())
         http.Error(w, "Internal Server Error", 500)
     }
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+    id, err := strconv.Atoi(r.URL.Query().Get("id"))
+    if err != nil || id < 1 {
+        http.NotFound(w, r)
+        return
+    }
+    fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+    if r.Method != http.MethodPost {
+        w.Header().Set("Allow", http.MethodPost)
+        w.WriteHeader(405)
+        w.Write([]byte("Method not allowed"))
+        return
+    }
+
+    w.Write([]byte("Create a new snippet..."))
 }
