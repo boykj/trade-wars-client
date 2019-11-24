5e359200d94557c1bb554bc7e91123a52893549b
boykj <jboyk918@gmail.com>
2019-11-24 00:36:41 -0800

    ship.go: Add basic variables for water and currency

--- internal/tradewars/ship.go
+++ internal/tradewars/ship.go
@@ -3,8 +3,12 @@ package main
 import "fmt"
 
 type ship struct {
+	// inventoy (map[key]value)
+	water_tank float64
+	currency uint64
 	fuel_tank float64
 	top_speed float64
+	current_speed float64
 }
 
 // func insert (inventory, item) {
@@ -12,7 +16,10 @@ type ship struct {
 // }
 
 func main(){
-	a_ship := ship{fuel_tank: 150.0,
-				   top_speed: 225.0}
-	fmt.Println(a_ship.fuel_tank)
+	a_ship := ship{water_tank: 75.0,
+				   currency: 0,
+				   fuel_tank: 150.0,
+				   top_speed: 225.0,
+				   current_speed: 0}
+	fmt.Println(a_ship)
 }
