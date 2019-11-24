6227753df8bb57d40bb4928e8b0f4da2ac1785bf
boykj <jboyk918@gmail.com>
2019-11-24 00:20:57 -0800

    ship.go: Add basic struct variables and test constructing

--- internal/tradewars/ship.go
+++ internal/tradewars/ship.go
@@ -3,7 +3,16 @@ package main
 import "fmt"
 
 type ship struct {
-	inventory = {}
-	fuel_tank = uint5
+	fuel_tank float64
+	top_speed float64
+}
 
+// func insert (inventory, item) {
+// 	inventory.append(item)
+// }
+
+func main(){
+	a_ship := ship{fuel_tank: 150.0,
+				   top_speed: 225.0}
+	fmt.Println(a_ship.fuel_tank)
 }
