6edee6048e02ace9357bbdbbbb2754f6ba84f11a
boykj <jboyk918@gmail.com>
2019-11-25 19:24:44 -0800

    main.go || handlers.go: Break stuff and back track
    Made some changes to function names for readability amidst
    merging. Had to backtrack and figured out the problem.

--- cmd/tradewars/handlers.go
+++ cmd/tradewars/handlers.go
@@ -6,7 +6,7 @@ import (
     "html/template"
 )
 
-func index(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
     if r.URL.Path != "/" {
         http.NotFound(w, r)
         return
