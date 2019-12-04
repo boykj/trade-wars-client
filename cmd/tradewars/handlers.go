a0f9b69a334e0854a9c7d0cd52ee5d4cf982ca89
boykj <48081661+boykj@users.noreply.github.com>
2019-12-04 14:16:50 -0800

    handlers.go: Implement tradeHandler with cookie variables

--- cmd/tradewars/handlers.go
+++ cmd/tradewars/handlers.go
@@ -117,30 +117,33 @@ func playerHandler(w http.ResponseWriter, r *http.Request) {
         w.Write([]byte("Welcome to Trade wars Chat, " + callsign))
  }
 
-//  func trade(w http.ResponseWriter, r *http.Request) {
-//     if r.URL.Path != "/" {
-//         http.NotFound(w, r)
-//         return
-//     }
+ func trade(w http.ResponseWriter, r *http.Request) {
+    files := []string{
+        "./ui/html/trade.page.tmpl",
+        "./ui/html/base.layout.tmpl",
+    }
 
-//     files := []string{
-//         "./ui/html/trade.page.tmpl",
-//         "./ui/html/base.layout.tmpl",
-//     }
+    ts, err := template.ParseFiles(files...)
+    if err != nil {
+        log.Println(err.Error())
+        http.Error(w, "Internal Server Error", 500)
+        return
+    }
 
-//     ts, err := template.ParseFiles(files...)
-//     if err != nil {
-//         log.Println(err.Error())
-//         http.Error(w, "Internal Server Error", 500)
-//         return
-//     }
-
-//     err = ts.Execute(w, nil)
-//     if err != nil {
-//         log.Println(err.Error())
-//         http.Error(w, "Internal Server Error", 500)
-//     }
-//  }
+    err = ts.Execute(w, nil)
+    if err != nil {
+        log.Println(err.Error())
+        http.Error(w, "Internal Server Error", 500)
+    }
+    var cookie, errC = r.Cookie("callsign")
+    if errC != nil {
+        log.Println(err.Error())
+        http.Error(w, "Internal Server Error || Cookie callsign not resolved", 500)
+        return
+    }
+    callsign := cookie.Value
+        w.Write([]byte("Welcome to Trade wars Trade Hub, " + callsign))
+ }
 
  func redirect(w http.ResponseWriter, r *http.Request) {
     if r.URL.Path == "/" {
